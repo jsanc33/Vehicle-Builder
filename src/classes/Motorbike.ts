15f234d01102893f1c8eca1cbc555e03907ca586
Joseph Sanchez <sanchezjoseph33@gmail.com>
2025-04-21 11:52:11 -0500

    addded comments to motorbikes

--- src/classes/Motorbike.ts
+++ src/classes/Motorbike.ts
@@ -2,7 +2,7 @@
 import Vehicle from './Vehicle.js';
 import Wheel from './Wheel.js';
 
-// TODO: The Motorbike class should extend the Vehicle class
+//The Motorbike class should extend the Vehicle class
 class Motorbike extends Vehicle {
   vin: string;
   color: string;
@@ -12,9 +12,9 @@ class Motorbike extends Vehicle {
   weight: number;
   topSpeed: number;
   wheels: Wheel[];
-  // TODO: Declare properties of the Motorbike class
-  // TODO: The properties should include vin, color, make, model, year, weight, top speed, and wheels
-  // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[])
+  //Declare properties of the Motorbike class
+  //The properties should include vin, color, make, model, year, weight, top speed, and wheels
+  //The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[])
   constructor(
     vin: string,
     color: string,
@@ -25,10 +25,10 @@ class Motorbike extends Vehicle {
     topSpeed: number,
     wheels: Wheel[],
   ) {
-  // TODO: Create a constructor that accepts the properties of the Motorbike class
-    // TODO: The constructor should call the constructor of the parent class, Vehicle
-    // TODO: The constructor should initialize the properties of the Motorbike class
-    // TODO: The constructor should check if the wheels array has 2 elements and create 2 new default Wheel objects if it does not
+  //Create a constructor that accepts the properties of the Motorbike class
+    //The constructor should call the constructor of the parent class, Vehicle
+    //The constructor should initialize the properties of the Motorbike class
+    //The constructor should check if the wheels array has 2 elements and create 2 new default Wheel objects if it does not
     super();
     this.vin = vin;
     this.color = color;
@@ -37,8 +37,8 @@ class Motorbike extends Vehicle {
     this.year = year;
     this.weight = weight;
     this.topSpeed = topSpeed;
-  // TODO: Implement the wheelie method
-    // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
+  //Implement the wheelie method
+    //The method should log the message "Motorbike [make] [model] is doing a wheelie!"
     if (wheels.length !== 2) {
       this.wheels = [new Wheel(), new Wheel()];
     } else {
